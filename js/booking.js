72a8d310e467a6abf96be8ec4224ed5a4e26d2d1
Olya <kormakova.96@mail.ru>
2024-11-13 10:38:20 +0300

    with corrections

--- js/booking.js
+++ js/booking.js
@@ -1,21 +1,22 @@
 const roomCart = document.querySelectorAll(".price-room__room");
-const buttonCart = document.querySelectorAll(".price-room__book-button");
-
-for (let i = 0; i < buttonCart.length; i++) {
-  buttonCart[i].addEventListener("click", function (evt) {
-    var bookCart = buttonCart[i].closest(".price-room__room")
-    // roomCart[i].addEventListener("mouseleave", function(evt) {
-      bookCart.classList.add("price-room__room--book");
-      // }
-    // )
-    }
-  )
-};
 
 for (let i = 0; i < roomCart.length; i++) {
-  roomCart[i].addEventListener("dblclick", function () {
-    roomCart[i].classList.remove("price-room__room--book");
+  var buttonCart = roomCart[i].querySelector(".price-room__book-button")
+  if (buttonCart !== null) {
+    buttonCart.addEventListener("click", function() {
+    roomCart[i].addEventListener("mouseleave", function addMouseLeave() {
+      roomCart[i].classList.add("price-room__room--book");
+      roomCart[i].removeEventListener("mouseleave", addMouseLeave);
     }
-  );
+    )
+    }
+    )
+  }
 }
 
+for (let i = 0; i < roomCart.length; i++) {
+  roomCart[i].addEventListener("click", function (evt) {
+    roomCart[i].classList.remove("price-room__room--book");
+  }
+  );
+}
